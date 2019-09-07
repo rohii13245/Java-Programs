c322adf87b0cbe495ad49c4928a6d1faadeb1cb4
Rohini Ahire <rohii13245@gmail.com>
2019-09-07 10:41:02 +0530

    this keyword

---
+++ src/main/java/thisKeyword/ThisKeyword2.java
@@ -0,0 +1,14 @@
+package thisKeyword;
+
+import thisKeyword.ThisKeyword;
+
+public class ThisKeyword2 {
+    public static void main(String[] args) {
+        ThisKeyword tk = new ThisKeyword();
+
+        tk.setinput(13,2,4);
+        tk.display();
+
+    }
+
+}
