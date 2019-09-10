2947ed39d7e2b6b8da4bb08b583e46d749254242
Rohini Ahire <rohii13245@gmail.com>
2019-09-10 16:22:20 +0530

    create and read file

---
+++ src/main/java/fileHandling/CreateFile.java
@@ -0,0 +1,28 @@
+package fileHandling;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CreateFile {
+    public static void main(String[] args) throws IOException {
+
+
+        FileOutputStream outputStream = new FileOutputStream("/home/codekul/Desktop/name/demo.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter  your msg : ");
+        byte[] bytes = scanner.nextLine().getBytes();
+        outputStream.write(bytes);
+        outputStream.close();
+
+        FileInputStream inputStream;
+        inputStream = new FileInputStream("/home/codekul/Desktop/name/demo.txt");
+        int i = 0;
+        while ((i = inputStream.read()) != -1) {
+            System.out.print((char) i);
+        }
+    }
+
+}
