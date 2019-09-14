f5ac88dfc394bcae51bd6145645846d2e6097deb
Rohini Ahire <rohii13245@gmail.com>
2019-09-14 10:57:30 +0530

    arithmatic exception

--- src/main/java/exceptionhandling/TestException.java
+++ src/main/java/exceptionhandling/TestException.java
@@ -1,4 +1,30 @@
 package exceptionhandling;
 
 public class TestException {
+
+    public static void main(String[] args) {
+        int num1,num2,ans;
+
+        try {
+            num1=0;
+            num2=10;
+
+            ans=num2/num1;
+
+            System.out.println(ans);
+
+        }catch (ArithmeticException e){
+
+            System.out.println("num can not divide by zero");
+
+        }catch (Exception e){
+
+            System.out.println("Exception can handle");
+        }
+
+        System.out.println("prog execution is done");
+
+
+    }
+
 }
